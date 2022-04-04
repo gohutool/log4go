fbfb09bf53e76f0c08098f5af98c1229be9a5af1
LiuYong <793875613@qq.com>
2022-04-04 23:03:39 +0800

    refactor a new version

---
+++ examples/configuration_test.go
@@ -0,0 +1,13 @@
+package examples
+
+/**
+* golang-sample源代码，版权归锦翰科技（深圳）有限公司所有。
+* <p>
+* 文件名称 : configuration_test.go
+* 文件路径 :
+* 作者 : DavidLiu
+× Email: david.liu@ginghan.com
+*
+* 创建日期 : 2022/4/4 22:35
+* 修改历史 : 1. [2022/4/4 22:35] 创建文件 by NST
+*/
